61e73d7a7656d532a070bfa3846b88d62a447312
José Victor Souza <142500862+jose6941@users.noreply.github.com>
2026-07-30 16:48:37 -0300

    Refactor Home component by removing unused code
    Removed unused imports and tRPC query from Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
 
   return (
     <HydrateClient>
@@ -39,13 +35,7 @@ export default async function Home() {
               </div>
             </Link>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
 
-          <LatestPost />
         </div>
       </main>
     </HydrateClient>
